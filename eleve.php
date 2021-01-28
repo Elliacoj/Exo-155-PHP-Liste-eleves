8b5cd6999a2fc325fd4d1bc0e573c7cfabb780c8
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-28 11:00:42 +0100

    Modif

---
+++ eleve.php
@@ -0,0 +1,39 @@
+<?php
+if(!isset($_GET['eleve'])) {
+    header('location: index.php?error=1');
+}
+else {
+    $eleveJSON = json_decode(base64_decode($_GET['eleve']));
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Eleve</title>
+</head>
+<body>
+
+    <?php
+        foreach ($eleveJSON as $item => $value) {
+            if(is_array($value)) {
+                $values = implode(", ", $value);
+                echo "<div>" . $item . ": " . $values . "</div>>";
+            }
+            else {
+                echo "<div>" . $item . ": " . $value . "</div>";
+            }
+        }
+        echo '<div><a href="index.php">Index</a></div>';
+    ?>
+
+</body>
+</html>
+
+<?php
+}
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$eleves = [ 0 => ["nom" => "Brian", "prenom" => "Dani", "age" => "45", "ville" => "Paris", "passions" => ["ski", "pétanque", "football"]],
+            1 => ["nom" => "Boon", "prenom" => "Jeff", "age" => "35", "ville" => "Bruxelles", "passions" => ["lecture", "basket", "mangas"]],
+            2 => ["nom" => "Plock", "prenom" => "Sarah", "age" => "22", "ville" => "Charleroi", "passions" => ["danse", "lecture", "politique"]],
+            3 => ["nom" => "Racklet", "prenom" => "Sylvie", "age" => "32", "ville" => "Anor", "passions" => ["chant", "tennis", "jardinnage"]],
+];
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
+</head>
+<body>
+    <div id="container">
+        <?php
+            foreach ($eleves as $eleve) {
+                $eleveJSON = base64_encode(json_encode($eleve));
+                echo "<div><a href='eleve.php?eleve=$eleveJSON'> $eleve[prenom] $eleve[nom]</a></div>";
+            }
+        ?>
+    </div>
+</body>
+</html>
